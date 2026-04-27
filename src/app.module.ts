fe3e82a8096baa7bca2e41b079adbc081b362d60
Ibeh Christogonus <christogonus@gmail.com>
2026-04-27 18:01:26 +0100

    feat: add card validation endpoint with Luhn algorithm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CardModule } from './card/card.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CardModule],
 })
 export class AppModule {}
